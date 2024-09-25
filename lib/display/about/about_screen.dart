7ae86191cf98bc9c6ce07daf0e067188d52e027c
markarian231 <ujpliki1@gmail.com>
2024-09-25 13:56:30 +0200

    add about screen

---
+++ lib/display/about/about_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:signal_flags_app/utils/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutScreen extends StatelessWidget {
+
+  // Function to launch URLs
+  void _launchURL(String url) async {
+    const url = 'https://msi.nga.mil/Publications/ICOS';
+  
+    // Convert string into Uri object
+    Uri uri = Uri.parse(url);
+  
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Flag Signals App', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 10),
+            const Text(
+              'Welcome to the Flag Signals App! This app is designed to help users learn about the signal flags used in maritime communication.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'As this is my first app, I wanted to create something educational and practical. '
+              'Signals with multiple flags used in this app were handpicked by me, based on what I believe to be the most commonly used ones. '
+              'You can find all of them and more information in the official document available here:',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            InkWell(
+              onTap: () {
+                _launchURL('https://msi.nga.mil/Publications/ICOS');
+              },
+              child: const Text(
+                'Official Signal Flags Handbook',
+                style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text('Contact', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 5),
+            const Text(
+              'I am trying to improve this app and expand its features. Your feedback is highly valued and appreciated, so please feel free to reach out!',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            const Text('email@example.com', style: TextStyle(color: Colors.blue)),
+            const SizedBox(height: 30),
+            TextButton(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  applicationName: 'Signal Flags App',
+                  applicationVersion: '1.0.0',
+                  applicationLegalese: '© 2024 Mikolaj N',
+                );
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 78, 78, 78),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              ),
+              child: const Text(
+                'More Info',
+                style: TextStyle(color: kWhiteColor),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/display/home/home_screen.dart
+++ lib/display/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:signal_flags_app/controllers/language_controller.dart';
+import 'package:signal_flags_app/display/about/about_screen.dart';
 import 'package:signal_flags_app/display/dataset/dataset_screen.dart';
 import 'package:signal_flags_app/display/quiz/quiz_menu_screen.dart';
 import 'package:signal_flags_app/display/signal/signal_menu_screen.dart';
@@ -24,27 +25,42 @@ class HomeScreen extends StatelessWidget {
           children: [
             SafeArea(
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         SizedBox(height: 10),
-                        GestureDetector(
-                          onTap: () {
-                            var locale = Locale('en', 'US');
-                            Get.updateLocale(locale);
-                            _languageController.setLanguage('en');
-                          },
-                          child: Image.asset(
-                            'assets/images/us_flag.png',
-                            width: 45,
-                            height: 30,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                var locale = Locale('en', 'US');
+                                Get.updateLocale(locale);
+                                _languageController.setLanguage('en');
+                              },
+                              child: Image.asset(
+                                'assets/images/us_flag.png',
+                                width: 45,
+                                height: 30,
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(
+                                Icons.info_outline_rounded,
+                                color: Colors.white,
+                                size: 30.0,
+                              ),
+                              onPressed: () {
+                                Get.to(() => AboutScreen());
+                              },
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 5),
+                        //SizedBox(height: 5),
                         GestureDetector(
                           onTap: () {
                             var locale = Locale('pl', 'PL');
@@ -64,37 +80,27 @@ class HomeScreen extends StatelessWidget {
                       children: [
                         Positioned(
                           top: -5,
-                          right: 30,
+                          right: 38,
                           child: Image.asset(
                             'assets/images/two_flags.png',
-                            width: 150, 
-                            height: 150, 
+                            width: 150,
+                            height: 150,
                           ),
                         ),
                         Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 80.0),
-                        child: Text(
-                          'home_screen.title'.tr,
-                          style: TextStyle(
-                            color: kHeaderColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 44,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 80.0),
+                            child: Text(
+                              'home_screen.title'.tr,
+                              style: const TextStyle(
+                                color: kHeaderColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 44,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
                       ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30.0),
-                      child: Text(
-                        'home_screen.choose_mode'.tr,
-                        style: const TextStyle(
-                          color: kHeaderColor,
-                          fontSize: 22,
-                        ),
-                      ),
                     ),
                     const Spacer(flex: 6),
                     BuildButtonWidget(text: 'home_screen.flag_names'.tr, getScreen: () => FlagsMenuScreen()), // widget instantiated only when user taps the button (GetX recommendation)
